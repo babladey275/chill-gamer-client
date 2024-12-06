4a42f205646f508a339612a5181e24ecabdf8daf
babladey275 <babladey275@gmail.com>
2024-12-06 17:08:42 +0600

    added register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,86 @@
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen my-6">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 border-2 rounded-md md:py-10 py-5">
+        <h2 className="text-2xl font-semibold text-center">
+          Register your account
+        </h2>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold">Your Name</span>
+            </label>
+            <input
+              name="name"
+              type="text"
+              placeholder="Enter Your name"
+              className="input input-bordered rounded-sm"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold">Photo URL</span>
+            </label>
+            <input
+              name="photo"
+              type="text"
+              placeholder="Enter your photo URL"
+              className="input input-bordered rounded-sm"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="Enter your email address"
+              className="input input-bordered rounded-sm"
+              required
+            />
+          </div>
+          <div className="form-control relative">
+            <label className="label">
+              <span className="label-text font-semibold">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+              className="input input-bordered rounded-sm"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-sm">Register</button>
+          </div>
+        </form>
+        <p className="text-center font-semibold text-gray-500">
+          Already Have An Account ?{" "}
+          <Link
+            to={"/auth/login"}
+            className="hover:underline font-bold text-black"
+          >
+            Login
+          </Link>
+        </p>
+        <div className="w-[86%] mx-auto">
+          <button className="btn btn-outline border-2 btn-neutral w-full rounded-sm mt-4 mx-auto">
+            <span>
+              <FaGoogle></FaGoogle>
+            </span>
+            Login With Google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
