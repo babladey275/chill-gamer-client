db8f6490bbb56b7110d38687dd4e76e38f5d7cd7
babladey275 <babladey275@gmail.com>
2024-12-07 22:04:51 +0600

    watchList data send to the server

--- src/pages/ReviewDetails.jsx
+++ src/pages/ReviewDetails.jsx
@@ -1,9 +1,50 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const ReviewDetails = () => {
   const review = useLoaderData();
+  const { user } = useContext(AuthContext);
   const { title, description, rating, genre, name, email } = review;
+
+  const handleAddToWatchList = () => {
+    const userName = user.displayName;
+    const userEmail = user.email;
+    const newWatchList = {
+      user: { userName, userEmail },
+      reviewDetails: {
+        title,
+        description,
+        rating,
+        genre,
+        name,
+        email,
+      },
+    };
+
+    //send data to the server
+    fetch("http://localhost:5000/watch-list", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newWatchList),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "WatchList added successfully!",
+            icon: "success",
+            confirmButtonText: "Ok",
+          });
+        }
+      });
+  };
 
   return (
     <div>
@@ -27,7 +68,12 @@ const ReviewDetails = () => {
               <p className="font-semibold">Genre: {genre}</p>
               <p className="font-semibold">Reviewer: {name || "Anonymous"}</p>
               <p className="font-semibold">Email: {email || "Not Provided"}</p>
-              <button className="btn btn-neutral mt-4">Add to WatchList</button>
+              <button
+                onClick={handleAddToWatchList}
+                className="btn btn-neutral mt-4"
+              >
+                Add to WatchList
+              </button>
             </div>
           </div>
         </div>
